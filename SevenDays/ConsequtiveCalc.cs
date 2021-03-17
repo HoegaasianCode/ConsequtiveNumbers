8e1a13ed34c7a20bda1f60e639c781b4488ca913
JoakimHoegaas <62018628+JoakimHoegaas@users.noreply.github.com>
2021-03-17 15:13:07 +0100

    Initial commit

---
+++ SevenDays/ConsequtiveCalc.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace ConsequtiveNumbers
+{
+    public class ConsequtiveCalc
+    {
+        private readonly int[] _conseqIntsA;
+        private readonly List<int> tempList = new List<int>();
+
+        public ConsequtiveCalc(int[] conseqInts)
+        {
+            _conseqIntsA = conseqInts;
+        }
+
+        public void SortArray()
+        {
+            Array.Sort(_conseqIntsA);
+        }
+
+        public void CreateMockList()
+        {
+            int x = _conseqIntsA[0];
+            tempList.Add(x);
+            int y = x - 1;
+            foreach (var i in _conseqIntsA)
+            {
+                ++y;
+                tempList.Add(y);
+            }
+            tempList.RemoveAt(1);
+        }
+
+        public void IsConsequtive()
+        {
+            if (Enumerable.SequenceEqual(tempList, _conseqIntsA)) Console.WriteLine("Yay");
+            else Console.WriteLine("Nay");
+        }
+    }
+}
---
+++ SevenDays/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsequtiveNumbers
+{
+    class Program
+    {
+        // https://edabit.com/challenge/TAZywz6R2hu9tDQWc
+        // SOLVED
+        // Awful performance wise - need to implement paralell looping
+
+        static void Main(string[] args)
+        {
+            var x = new ConsequtiveCalc(new int[] { 5, 4, 3, 2, 1 });
+            x.SortArray();
+            x.CreateMockList();
+            x.IsConsequtive();
+        }
+    }
+}
